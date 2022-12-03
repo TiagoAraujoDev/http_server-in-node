0587a528acb427cb9ad9a87019c3dfa04c7e27c3
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-12-03 20:39:58 +0000

    feat: logout

---
+++ src/controllers/users/logoutUserController.js
@@ -0,0 +1,54 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const fsPromises = require("node:fs/promises");
+const path = require("node:path");
+
+const usersDB = {
+  users: require("../../models/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  }
+};
+
+const handleLogout = async (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) {
+    return res.sendStatus(204);
+  }
+
+  const refreshToken = cookies.jwt;
+
+  const { sub: userId } = jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET
+  );
+
+  const user = usersDB.users.find((user) => user.id === userId);
+
+  if (!user) {
+    res.clearCookie(jwt, { httpOnly: true, sameSite: "None", secure: true });
+    return res.sendStatus(204);
+  }
+
+  const newUsersDBData = usersDB.users.map((user) => {
+    if (user.id === userId) {
+      return { ...user, refresh_token: "" };
+    }
+
+    return user;
+  });
+
+  usersDB.setUsers([...newUsersDBData]);
+
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "..", "models", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie(jwt, { httpOnly: true, sameSite: "None", secure: true });
+  return res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
--- src/routes/api/users.routes.js
+++ src/routes/api/users.routes.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const registerUsersController = require("../../controllers/users/registerUsersController");
 const authUserController = require("../../controllers/users/authUserController");
 const refreshTokenController = require("../../controllers/users/refreshTokenController");
+const logoutUserController = require("../../controllers/users/logoutUserController");
 
 const usersRoutes = Router();
 
 usersRoutes.post("/register", registerUsersController.handleNewUser);
 usersRoutes.post("/auth", authUserController.handleAuthUser);
 usersRoutes.get("/refresh-token", refreshTokenController.handleRefreshToken);
+usersRoutes.get("/logout", logoutUserController.handleLogout);
 
 module.exports = usersRoutes;
