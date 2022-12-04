a718a1091881f1611f44286197ec503c6431bac0
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-12-04 22:55:52 +0000

    refactor: user registration to mongodb

--- src/controllers/users/registerUsersController.js
+++ src/controllers/users/registerUsersController.js
@@ -1,14 +1,5 @@
 const bcrypt = require("bcrypt");
-const { v4: uuid } = require("uuid");
-const fsPromises = require("node:fs/promises");
-const path = require("node:path");
-
-const usersDB = {
-  users: require("../../models/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  }
-};
+const User = require("../../models/User");
 
 const handleNewUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -19,7 +10,7 @@ const handleNewUser = async (req, res) => {
       .json({ message: "Name, email and password are required!" });
   }
 
-  const userAlredyExist = usersDB.users.some((user) => user.email === email);
+  const userAlredyExist = await User.findOne({ email: email }).exec();
 
   if (userAlredyExist) {
     return res.sendStatus(409);
@@ -28,22 +19,17 @@ const handleNewUser = async (req, res) => {
   try {
     const hashedPwd = await bcrypt.hash(password, 8);
 
-    const newUser = {
-      id: uuid(),
+    const newUser = new User({
       name,
       email,
-      password: hashedPwd,
-      roles: { user: 2001 }
-    };
+      password: hashedPwd
+    });
 
-    usersDB.setUsers([...usersDB.users, newUser]);
+    console.log(newUser);
 
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "..", "models", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
+    const result = await newUser.save();
 
-    return res.status(201).json({ newUser });
+    return res.status(201).json({ result });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
